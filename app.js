bb6899b3c1ac0ce376485205744354852268b8b9
fancyers <kjng5901@gmail.com>
2022-03-09 17:11:44 +0900

    로그 메시지에서 비어있는 GET 제거

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: false }))
 
 // Request log
 app.use((req, res, next) => {
+    if (req.originalUrl === '/') return next()
     console.log(
         'Request URL:',
         `[${req.method}]`,
