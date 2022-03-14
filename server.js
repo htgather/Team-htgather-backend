063d56dd1879450070ef84d4bd78160f34ef6d05
fancyers <kjng5901@gmail.com>
2022-03-15 01:09:40 +0900

    CI/CD 적용 테스트

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const { server } = require('./socket')
 const port = process.env.PORT
 
 server.listen(port, () => {
-    console.log('http server on', port, 'CI/CD 적용 테스트')
+    console.log('http server on', port, 'CI/CD 적용 테스트!')
 })
