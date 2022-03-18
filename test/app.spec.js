a32481e93b4d45315f00260d69c0930903f591ae
fancyers <kjng5901@gmail.com>
2022-03-18 10:06:30 +0900

    테스트 코드

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,20 +3,44 @@ const request = require('supertest')
 const chai = require('chai')
 const expect = chai.expect
 const User = require('../models/user')
+const WorkOutTime = require('../models/workOutTime')
+let token
+let userId
 
-describe('로그인 테스트', () => {
-    let token
+describe('유저 정보 테스트', async () => {
+    afterEach(async () => {
+        await User.deleteOne({ snsId: 123 })
+        token = undefined
+    })
 
-    it('로그인 성공 시 token을 받는다.', async () => {
+    it('snsId와 nickName을 입력하고, DB에 snsId에 해당하는 회원이 없으면 회원가입한다.', async () => {
+        await User.deleteOne({ snsId: 123 })
         const response = await request(app).post('/users/auth').send({
             snsId: 123,
             nickName: 'abc',
         })
-        token = response.body.token
+        const existUser = await User.findOne({ snsId: 123 })
+        expect(existUser).to.exist
         expect(response.status).equal(200)
     })
 
-    it('회원 정보 수정 시 수정된 내용이 DB에 적용되고 token을 받는다.', async () => {
+    it('snsId와 nickName을 입력하고, DB에 snsId에 해당하는 회원이 있으면 로그인한다.', async () => {
+        await User.create({ snsId: 123, nickName: 'abc' })
+        const response = await request(app).post('/users/auth').send({
+            snsId: 123,
+            nickName: 'abc',
+        })
+        expect(response.status).equal(200)
+    })
+
+    it('nickName과 weeklyGoal을 입력하면 회원 정보가 수정된다.', async () => {
+        token = (
+            await request(app).post('/users/auth').send({
+                snsId: 123,
+                nickName: 'abc',
+            })
+        ).body.token
+
         const response = await request(app)
             .patch('/users')
             .set('Authorization', 'Bearer ' + token)
@@ -24,9 +48,47 @@ describe('로그인 테스트', () => {
                 nickName: 'modified',
                 weeklyGoal: 5,
             })
-        console.log(response)
-
-        token = response.body.token
         expect(response.status).equal(200)
+        const existUser = await User.findOne({ snsId: 123 })
+        expect(existUser.nickName).equal('modified')
+        expect(existUser.weeklyGoal).equal(5)
+    })
+})
+
+describe('운동 기록 테스트', () => {
+    beforeEach(async () => {
+        const response = await request(app).post('/users/auth').send({
+            snsId: 123,
+            nickName: 'abc',
+        })
+        token = response.body.token
+        const existUser = await User.findOne({ snsId: 123 })
+        userId = existUser.userId
+    })
+
+    afterEach(async () => {
+        await User.deleteOne({ snsId: 123 })
+        token = undefined
+        userId = undefined
+    })
+
+    it('운동 기록에는 userId, workOutTime, category가 기록된다.', async () => {
+        const response = await request(app)
+            .post('/myinfo/records')
+            .set('Authorization', 'Bearer ' + token)
+            .send({
+                workOutTime: 30,
+                category: '요가',
+            })
+        expect(response.status).equal(200)
+        const oneRecord = await WorkOutTime.findOne({ userId })
+        expect(oneRecord.workOutTime).equal(30)
+        expect(oneRecord.category).equal('요가')
+        await WorkOutTime.deleteOne({ userId })
+    })
+
+    it('내가 일주일에 몇 일 운동했는지 알 수 있다.', () => {
+        // 가짜 데이터 만들기
+        // 일주일 중 랜덤 날짜 5개
     })
 })
